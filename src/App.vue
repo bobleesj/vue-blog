e51e3b697ffd727044a774d9b561cd117ca3135b
Bob Lee <bob@bobthedeveloper.io>
2017-09-10 16:56:09 +0900

    Learning night watch

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <ul>
-    </ul>
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
     <router-link to="/team">Team</router-link>
