f938dd9cec423ee43165e7c5cad87ca022095355
Bob Lee <bob@bobthedeveloper.io>
2017-09-10 21:56:03 +0900

    Added v model and rendering markdown

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -3,6 +3,7 @@
   <div class="team">
     <h1>Team</h1>
     <div v-for="member in members">
+      <div v-html="member.img" />
       <h2>{{ member.name }}</h2>
       <h3>{{ member.role }}</h3>
       <p>{{ member.description }}</p>
@@ -20,17 +21,20 @@ export default {
         {
           name: 'Bob',
           role: 'Content Distributor in Spanish',
-          description: 'Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change.'
+          description: 'Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change.',
+          img: '<img src="https://www.bobthedeveloper.io/img/profiles/team/raphael-profile.png" />'
         },
         {
           name: 'Bobby',
           role: 'Content Distributor in Spanish',
-          description: 'Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change.'
+          description: 'Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change.',
+          img: '<img src="https://www.bobthedeveloper.io/img/profiles/team/raphael-profile.png" />'
         },
         {
           name: 'Banana',
           role: 'Content Distributor in Spanish',
-          description: 'Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change'
+          description: 'Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change',
+          img: '<img src="https://www.bobthedeveloper.io/img/profiles/team/raphael-profile.png" />'
         }
       ]
     }
@@ -38,12 +42,15 @@ export default {
 }
 </script>
 
-
+<!--  SCSS  -->
 <style scoped>
-
 ul {
   list-style-type: none;
   padding: 0;
+}
+
+img {
+  width: 100px;
 }
 
 li {
--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -5,31 +5,40 @@
     <p>Be able to upload markdown and good articles</p>
     <textarea v-model="markdown" class="markdown" placeholder="Enter Markdown"></textarea>
     <p>{{markdown}}</p>
+    <div v-html="renderedMarkdown">
+    </div>
   </div>
 </template>
 
 
 <!--  Vue  -->
 <script>
+import showdown from 'Showdown'
+var converter = new showdown.Converter()
+
 export default {
   name: 'upload',
   data () {
     return {
       markdown: 'Markdown'
     }
+  },
+  computed: {
+    renderedMarkdown: function () {
+      var text = this.markdown
+      var html = converter.makeHtml(text)
+      return html
+    }
   }
 }
 </script>
 
 
-
 <!--  SCSS  -->
 <style lang="scss">
-
 .markdown {
   width: 80%;
   height: 200px;
   font-size: 18px;
 }
-
 </style>
