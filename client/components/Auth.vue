92f5eca93de208777025eac9a5bcb93ee24682f3
Bob Lee <bob@bobthedeveloper.io>
2017-09-12 16:12:53 +0900

    Added pug for template

--- client/components/Auth.vue
+++ client/components/Auth.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="auth">
-    <h1>Login</h1>
-    <h2>Join in 2 seconds</h2>
-    <form>
-      <input placeholder="Email" id="email"/>
-      <input placeholder="Password" id="password"/>
-      <button>Join</button>
-    </form>
+    <!--  Login Header -->
+    <div class="auth-header">
+      <h1>Login</h1>
+      <h2>Join in 2 seconds</h2>
+    </div>
+    <!--  Email/Password Form-->
+    <div class="form">
+      <form>
+        <input placeholder="Email" id="email"/>
+        <input placeholder="Password" id="password"/>
+        <button>Join</button>
+      </form>
+    </div>
     <div>
       <div id="left-divider"></div>
       <span id="divider-text">OR</span>
       <div id="right-divider"></div>
     </div>
     <!--  Facebook Join -->
-
   </div>
 </template>
--- client/components/Blog.vue
+++ client/components/Blog.vue
@@ -1,5 +1,6 @@
-<template>
-  <div class="blog">
-    <h1>Blog</h1>
-  </div>
+<template lang="pug">
+  .auth
+    h1 Blog
+    h2 Bob the Developer
+    h3 Learn Swift with Bob
 </template>
