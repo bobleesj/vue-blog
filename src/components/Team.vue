b9430fbfdbb328ebbf206f822e1bbd95b91046ae
Bob Lee <bob@bobthedeveloper.io>
2017-09-14 15:34:56 +0900

    Binding of in team works

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,72 +1,65 @@
 <!--  Template  -->
-<template>
-  <div class="team">
-    <h1>Team</h1>
-    <div v-for="member in members">
-      <div v-html="member.img" />
-      <h2>{{ member.name }}</h2>
-      <h3>{{ member.role }}</h3>
-      <p>{{ member.description }}</p>
-    </div>
-  </div>
+<template lang="pug">
+.team
+  .team-header
+    h1 Team Bob the Developer
+    h2 We work together to bring top-quality content
+  .team-member(v-for="member in members")
+     .team-member-profile
+       img(:src="member.img")
+     .team-member-title
+       h3 {{ member.name }}
+       h4 {{ member.role }}
+     .team-member-social
+       a(:src="member.twitter")
+     .team-member-description
+       p(v-html="member.description")
 </template>
 
 <!--  Vue  -->
-<script>
+<script scope>
 export default {
   data () {
     return {
       name: 'Team',
       members: [
         {
-          name: 'Bob',
+          name: 'Raphael 🇺🇸',
           role: 'Content Distributor in Spanish',
-          description: 'Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change.',
-          img: '<img src="https://www.bobthedeveloper.io/img/profiles/team/raphael-profile.png" />'
+          description: `Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change. <br />
+          On a whim one day, I decided to try and make an iOS game not knowing anything about development. I quickly learned how in over my head I was, but what kept me going was my immediate love for Swift and coding itself. I love the strain of working through challenging problems and the elation of finally breaking through. I love helping people become better programmers, and that is what led me to Bob. Working with Bob has allowed me to help people change their work, their lives, and even the world all through doing what I love`,
+          twitter: 'https://www.twitter.com/bobleesj',
+          linkedin: 'https://www.linkedin.com/bobthedev',
+          github: 'https://www.github.com/bobthedev',
+          img: `https://www.bobthedeveloper.io/img/profiles/team/raphael-profile.png`
         },
         {
           name: 'Bobby',
           role: 'Content Distributor in Spanish',
-          description: 'Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change.',
-          img: '<img src="https://www.bobthedeveloper.io/img/profiles/team/raphael-profile.png" />'
+          description: `Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change. <br />
+          On a whim one day, I decided to try and make an iOS game not knowing anything about development. I quickly learned how in over my head I was, but what kept me going was my immediate love for Swift and coding itself. I love the strain of working through challenging problems and the elation of finally breaking through. I love helping people become better programmers, and that is what led me to Bob. Working with Bob has allowed me to help people change their work, their lives, and even the world all through doing what I love`,
+          twitter: 'https://www.twitter.com/bobleesj',
+          linkedin: 'https://www.linkedin.com/bobthedev',
+          github: 'https://www.github.com/bobthedev',
+          img: `https://www.bobthedeveloper.io/img/profiles/team/raphael-profile.png`
         },
         {
           name: 'Banana',
           role: 'Content Distributor in Spanish',
-          description: 'Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change',
-          img: '<img src="https://www.bobthedeveloper.io/img/profiles/team/raphael-profile.png" />'
+          description: `Born and raised in California. I moved up to Oregon in 2010 to study psychology and Spanish at the University of Oregon. While in college, I thought for sure I was going to be a therapist, but after taking multiple courses in psychology and hating most of them I knew something had to change. <br />
+          On a whim one day, I decided to try and make an iOS game not knowing anything about development. I quickly learned how in over my head I was, but what kept me going was my immediate love for Swift and coding itself. I love the strain of working through challenging problems and the elation of finally breaking through. I love helping people become better programmers, and that is what led me to Bob. Working with Bob has allowed me to help people change their work, their lives, and even the world all through doing what I love`,
+          twitter: 'https://www.twitter.com/bobleesj',
+          linkedin: 'https://www.linkedin.com/bobthedev',
+          github: 'https://www.github.com/bobthedev',
+          img: `https://www.bobthedeveloper.io/img/profiles/team/raphael-profile.png`
         }
       ]
     }
-  },
-  methods: {
-    updateImg: function () {
-      console.log('updateImg called')
-    }
-  },
-  created: function () {
-    this.name = 'Bob Lee'
   }
 }
 </script>
 
 <!--  SCSS  -->
-<style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-img {
-  width: 100px;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style lang="scss">
+@import '../styles/team.scss';
 </style>
