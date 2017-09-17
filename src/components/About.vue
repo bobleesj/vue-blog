6df56933acd0a8c5f7d0877a41e1fe6236300745
Bob Lee <bob@bobthedeveloper.io>
2017-09-17 23:40:06 +0900

    Getting started with mobile version of about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,135 @@
-<template>
-  <div class="about">
-    <h1>About</h1>
-  </div>
+<template lang="pug">
+.about
+  .about-introduction
+    .about-introduction-profile
+      img(src="../assets/bob-full-body.png")
+    .about-introduction-bio
+      h1 Bob Lee
+      p My name is Bob. I'm a full-stack developer, good enough to build websites and iOS apps with reactive programming and front-end development. I distribute content as the editor of the iOS Developers Facebook Page and make a living as an online instructor and blogger.
+
+      p In 1996, I was born in Seoul. Due to my father's international business, I was fortunate to experience a variety of cultures. I primarily lived in Penang, Malaysia and Ho Chi Minh City, Vietnam and attended a number of international schools. In college, I studied chemical engineering at Cooper Union located in Manhattan. On Sept 16, 2016, I decided to stop pursuing the engineering degree, and instead get involved with teaching and sharing what I've learned so far, utlimately working towards providing affordable and quality education.
+
+      p My short-term goal as an instructor is to raise fund to pursue furthur education in the areas of computer science and electrical engineering at a higher level. I'm determined to building a product that combines the VR technology along with education. I believe education should be accessible by all people regardless of wealth, race, gender, and any other factors. VR and AR are on the way to disrupt the way knowledge is distributed. I wish I could be a part of the change.
+  .about-featured
+    .about-featured-header
+      h3(id="featured-text") As Featured In
+    .about-featured-logos
+      img(src="../assets/logos/realm-logo.png" id="realm")
+      img(src="../assets/logos/udemy-logo.png" id="udemy")
+      img(src="../assets/logos/medium-logo.png" id="medium")
+      img(src="../assets/logos/ios-goodies-logo.png" id="ios-goodies")
+      img(src="../assets/logos/ios-dev-weekly-logo.png" id="ios-dev-weekly")
+      img(src="../assets/logos/this-week-in-swift-logo.png" id="this-week-swift")
+
+  //- Recommenation -//
+  .about-recommendation
+    h3(id="recommendation-text") Recommendations
+    h4(id="recommendation-subtext") What others have said about my work
+    .about-recommendation-individual
+      .about-recommendation-individual-header
+        .about-recommendation-individual-header-profile
+          img(src="../assets/review-profiles/alex.png")
+        .about-recommendation-individual-header-description
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
+          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+      .about-recommendation-individual-content
+        p I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.
+
+
+    .about-recommendation-individual
+      .about-recommendation-individual-header
+        .about-recommendation-individual-header-profile
+          img(src="../assets/review-profiles/alex.png")
+        .about-recommendation-individual-header-description
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
+          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+      .about-recommendation-individual-content
+        p I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.
+
+
+    .about-recommendation-individual
+      .about-recommendation-individual-header
+        .about-recommendation-individual-header-profile
+          img(src="../assets/review-profiles/alex.png")
+        .about-recommendation-individual-header-description
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
+          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+      .about-recommendation-individual-content
+        p I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.
+
+
+    .about-recommendation-individual
+      .about-recommendation-individual-header
+        .about-recommendation-individual-header-profile
+          img(src="../assets/review-profiles/alex.png")
+        .about-recommendation-individual-header-description
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
+          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+      .about-recommendation-individual-content
+        p I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.
+
+    .about-recommendation-individual
+      .about-recommendation-individual-header
+        .about-recommendation-individual-header-profile
+          img(src="../assets/review-profiles/alex.png")
+        .about-recommendation-individual-header-description
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
+          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+      .about-recommendation-individual-content
+        p I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.
+
+    .about-recommendation-individual
+      .about-recommendation-individual-header
+        .about-recommendation-individual-header-profile
+          img(src="../assets/review-profiles/alex.png")
+        .about-recommendation-individual-header-description
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
+          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+      .about-recommendation-individual-content
+        p I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.
+
+    .about-recommendation-individual
+      .about-recommendation-individual-header
+        .about-recommendation-individual-header-profile
+          img(src="../assets/review-profiles/alex.png")
+        .about-recommendation-individual-header-description
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
+          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+      .about-recommendation-individual-content
+        p I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.
+
+    .about-recommendation-individual
+      .about-recommendation-individual-header
+        .about-recommendation-individual-header-profile
+          img(src="../assets/review-profiles/alex.png")
+        .about-recommendation-individual-header-description
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
+          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+      .about-recommendation-individual-content
+        p I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.
+
+    .about-recommendation-individual
+      .about-recommendation-individual-header
+        .about-recommendation-individual-header-profile
+          img(src="../assets/review-profiles/alex.png")
+        .about-recommendation-individual-header-description
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
+          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+      .about-recommendation-individual-content
+        p I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.
+
+
+
+
+
+
+  .about-footer
+    span I can be reached at #[a(href="https://team.bobthedeveloper.io" target="_blank") bob@bobthedeveloper.io]
+
+
 </template>
+
+
+<style lang="scss">
+@import '../styles/about.scss';
+</style>
