a2f6ff6c53070b574f8a8acb155f6f64604a8ffc
Bob Lee <bob@bobthedeveloper.io>
2017-09-18 09:48:29 +0900

    Completed recommendation

--- src/components/About.vue
+++ src/components/About.vue
@@ -30,8 +30,8 @@
         .about-recommendation-individual-header-profile
           img(:src="review.img")
         .about-recommendation-individual-header-description
-          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") Alex McPherson]
-          span#title iOS Developer Manager @#[a(href="http://www.my-mo.co.uk/" target="_blank") my-mo]
+          span#name #[a(href="https://www.linkedin.com/in/flexmcpee/") {{ review.name }}]
+          span(v-html="review.title" id="title")
       .about-recommendation-individual-content
         p {{ review.message }}
 
@@ -64,15 +64,67 @@ export default {
       reviews: [
         {
           name: `Alex McPherson`,
-          title: `iOS Developer @my-mo`,
+          title: `iOS Developer Manager <a href="https://github.com/my-mo">my-mo</a>`,
           message: `I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.`,
+          website: `https://www.linkedin.com/in/flexmcpee/`,
           img: `https://cdn.bobthedeveloper.io/assets/reviews/alex-min.png`
         },
         {
-          name: `Alex McPherson`,
-          title: `iOS Developer @my-mo`,
-          message: `I have been building apps for iOS for about 6-7 years with my first language being Objective-C. I never felt I've mastered the language with the release of Swift. In the past, I found many resources online some were alright and some were terrible turning us into code monkeys without any understanding what was seen. With the help of Bob's intermediate to advnaced Swift course, I have been able to grasp the language way better than I could without his help. His awesome teaching style, approachability and the way Bob uses real world analogies to get a subject across are magnificent. I wouldn’t hesitate to recommend Bob's course to anyone who wishes to learn the language of Swift because that exactly what you will do.`,
-          img: `https://cdn.bobthedeveloper.io/assets/reviews/alex-min.png`
+          name: `Byron BP`,
+          title: `QA Engineer`,
+          message: `I have completed Bob's intermediate to Advanced Swift course. As the course title states, you really learn Swift with Bob. The concepts that are carried out in this course can become quite dense, complex, possibly boring. The way it’s been taught by Bob, on the other hand, removes all. There is nothing that is not understood by the student. Even if you have doubts, you can contact him through email or Slack and solve the problem almost immediately. One of the aspects that differentiates Bob's course with any other online coursres is the documentation that Bob provides. Incredibly useful to follow the classes as well to come back any time to consult some specific concept. if you want to take your Swift to the next level, I recommend starting with Bob. He will get you there.`,
+          website: `https://www.linkedin.com/in/byron-bacusoy-pinela-7690b7149/"`,
+          company: `QA Engineer`,
+          img: `https://cdn.bobthedeveloper.io/assets/reviews/byron-min.png`
+        },
+        {
+          name: `Oren Alalouf`,
+          title: `Lead SCCM @<a href="https://www.linkedin.com/company-beta/24646/">SQLink Group</a>`,
+          message: `For every traveller, in the beginning , there is the mentor/source material for the journey ahead. I happened to stumble onto Bob's writing on his blog, and I can't say enough about how awesome the output is. From the first post – the agenda was nothing more than to enrich the reader, handled with a knowledgeable and most of all – human and relatable delivery. If I only had Bob's materials when I started my journey into iOS development, the road ahead would have been shorter, clearer and most certainly more enjoyable. I have the distinct pleasure of calling Bob a colleague. Thank you Bob for your persistent support and contribution to the iOS community. It's people like you who give back make learning more fun and the challenge more manageable.`,
+          website: `https://www.linkedin.com/in/orenalalouf/`,
+          img: `https://cdn.bobthedeveloper.io/assets/reviews/oren-min.png`
+        },
+        {
+          name: `Patrick Bellot`,
+          title: `Front End Web Developer @<a href="https://www.linkedin.com/in/gpatrickbellot">Polestar</a>`,
+          message: `I was introduced to Bob from his in-depth blog posts on Medium. Since then, Bob has grown as an instructor and developer, creating one of the most popular Swift courses on the Udemy platform.  Bob’s explanations of concepts in Swift are easy to follow. If anyone wants to learn iOS and Swift, I immediately direct them to Bob's content. I know Bob will help them understand Swift and be ready for a career in iOS mobile development. Bob is also an inspiration to me and others. He has dedicated his personal time and resources to teach Swift. By focusing, understanding, and mastering core concepts through teaching, Bob has successfully built a reputation as an expert.`,
+          website: `https://twitter.com/pbellot`,
+          img: `https://cdn.bobthedeveloper.io/assets/reviews/patrick-min.png`
+        },
+        {
+          name: `Luna An`,
+          title: `The Flatiron Graduate • iOS Engineering Intern @<a href="https://www.prolificinteractive.com/">Prolific Interactive</a>`,
+          message: `Bob is such an amazing instructor. His Medium blog has been incredibly resourceful during my journey of iOS development, and I'm so thankful for his recent creation of this well structured, excellent Udemy course. You will have lots of fun with Bob while taking this course and I would say his passion for iOS development is at an all time high - it is just so clear that he really loves what he is doing, and that shows in his teaching! I'd heartily recommend this course to anyone who wants to be a better Swift programmer. Thanks, Bob. You rock.`,
+          website: `https://www.linkedin.com/in/mimicatcodes/`,
+          img: `https://cdn.bobthedeveloper.io/assets/reviews/luna-min.png`
+        },
+        {
+          name: `Justin Stanley`,
+          title: `Swift iOS/React Native Developer at @<a href="https://www.skipthedishes.com/">SkipTheDishes</a>`,
+          message: `Bob can always take a confusing or complex subject and break it down into easily digestible explanations that are comprehensive and logically-structured. His examples and correlations always help my brain learn core concepts better—even those that I thought I knew well enough already. Bob’s teachings have made me a stronger and more confident developer`,
+          website: `https://twitter.com/JStheoriginal`,
+          img: `https://cdn.bobthedeveloper.io/assets/reviews/justin-min.png`
+        },
+        {
+          name: `Nam-Anh Vu`,
+          title: `Learning iOS Development`,
+          message: `Bob is a very talented and unique individual. His online tutorials have greatly expanded my Swift knowledge, fast. Simple real life examples are how I learn, and Bob breaks concepts down concisely. Bob is a great teacher who has a vision to become a unique creator in life and to help others in the process. I have a solid feeling he will succeed in doing just that!`,
+          website: `https://twitter.com/yoowinks`,
+          img: `https://cdn.bobthedeveloper.io/assets/reviews/nammy-min.png`
+        },
+        {
+          name: `Joshua Ziering`,
+          title: `Founder @<a href="https://kittyhawk.io/">Kitty Hawk</a>`,
+          message: `Although I am relatively new to Swift, after having read through Bob's blog posts and tutorials, I've begun to appreciate Bob's teaching philosophy and attention to detail. It would be have been great if Bob also wrote tutorials on a langauge I currently use. Maybe, I should just learn Swift instead. `,
+          website: `https://www.linkedin.com/in/joshziering/`,
+          img: `https://cdn.bobthedeveloper.io/assets/reviews/joshua-min.png`
+        },
+        {
+          name: `Oren Alalouf`,
+          title: `Musician and iOS Developer @<a href="http://www.belatrixsf.com/">Belatrix Software</a>`,
+          message: `FI've been a self-taught iOS developer for 6 years and since I took Bob's course my Swift skills took a boost of productivity. I started to take really advantage of Swift strengths and my projects started to become easier to take along. I couldn't be more grateful to Bob.`,
+          website: `https://www.linkedin.com/in/leandrofournier/`,
+          img: `https://cdn.bobthedeveloper.io/assets/reviews/leandro-min.png`
         }
       ],
       events: [
