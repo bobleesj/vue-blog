27977a03643b303323219d5bb7075e12c8731d57
Bob Lee <bob@bobthedeveloper.io>
2017-09-28 19:08:31 +0900

    Change header based on token

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
   <div id="app" v-on:click="pressedBody">
+
     <nav role="navigation">
-      <!--  Side navigation pop up -->
+      <!--  Side nav -->
       <div id="sideNav" class="sidenav" ref="sideNav">
         <a href="javascript:void(0)" class="closebtn">Close</a>
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-        <router-link to="/team">Team</router-link>
-        <router-link to="/upload">Upload</router-link>
-        <router-link to="/auth">Login</router-link>
+        <!-- The user is logged in -->
+        <div v-if="isLoggedIn">
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+          <router-link to="/team">Team</router-link>
+          <router-link to="/user">Profile</router-link>
+        </div>
+        <div v-else>
+          <!--  The user is not logged in -->
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+          <router-link to="/team">Team</router-link>
+          <router-link to="/auth">Login</router-link>
+        </div>
       </div>
       <!--  Open button-->
       <div class="hamburger">
@@ -19,16 +29,34 @@
       <!--  Logo  -->
       <div class="logo-container">
         <a id="logo" href="/"><img src="./assets/logo.png"/></a>
-      </div>
-      <div id="topNav" class="topNav" ref="topNav">
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-        <router-link to="/team">Team</router-link>
-        <router-link to="/upload">Upload</router-link>
-        <router-link to="/auth">Login</router-link>
+        <!--  Side navigation when the user is not logged in -->
       </div>
 
+      <!--  Top nav-->
+      <div id="topNav" class="topNav" ref="topNav">
+        <!-- The user is logged in -->
+        <div v-if="isLoggedIn">
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+          <router-link to="/team">Team</router-link>
+          <router-link to="/upload">Upload</router-link>
+          <router-link to="/user">Profile</router-link>
+        </div>
+        <div v-else>
+          <!--  The user is not logged in -->
+          <router-link to="/">Home</router-link>
+          <router-link to="/about">About</router-link>
+          <router-link to="/team">Team</router-link>
+          <router-link to="/upload">Upload</router-link>
+          <router-link to="/auth">Login</router-link>
+        </div>
+
+      </div>
     </nav>
+
+
+
+
     <router-view></router-view>
   </div>
 </template>
@@ -38,18 +66,26 @@ export default {
   name: 'app',
   data: function () {
     return {
-      isNavOpen: false
+      isLoggedIn: false
+    }
+  },
+  created () {
+    console.log('Component created')
+    if (this.$cookie.get(`token`)) {
+      this.isLoggedIn = true
     }
   },
   methods: {
     pressedBody: function (event) {
-      console.log(this.$cookie.get('token'))
-      console.log(event)
+      // Press other area, close the sidenav
       if (event.target.id === 'first' || event.target.id === 'second' || event.target.id === 'third' || event.target.className === 'hamburger') {
         this.$refs.sideNav.style.width = `180px`
       } else {
         this.$refs.sideNav.style.width = `0px`
       }
+    },
+    changeNavBasedOnToken: function () {
+      console.log('Working')
     }
   }
 }
--- src/components/User.vue
+++ src/components/User.vue
@@ -6,17 +6,41 @@
         span Change Image
       .profile-container-info
         form(action="/api/auth/signup" method="post")
+          span User ID
+          input(:value="userId" readonly)
           span Full Name
-          input(placeholder="Full Name" value="Bob Lee")
-          span Tags
-          input(placeholder="swift, ios, guitar, node.js")
-          span Country
-          input(placeholder="Country" name="country")
-          span Biography
+          input(placeholder="Full Name" :value="name")
+          span Description
           textarea(placeholder="Tell us more about your expertise and interest" name="biography")
+          span Token
+          input(:value="token")
           button Edit
+          p {{ posts}}
 </template>
 
+<script>
+import axios from 'axios'
+
+export default {
+  data: function () {
+    return {
+      name: `Bob Lee`,
+      userId: `12312312`,
+      token: this.$cookie.get('token'),
+      posts: [``]
+    }
+  },
+  async created () {
+    try {
+      const response = await axios.get(`http://localhost:5000/blog/posts`)
+      this.posts.push(response)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-@import '../styles/profile';
+@import '../styles/user';
 </style>
