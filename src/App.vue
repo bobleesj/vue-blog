7d9343e0e5f992e8d302b786a1e2a4bc6d3b8fd5
Bob Lee <bob@bobthedeveloper.io>
2017-09-28 11:25:19 +0900

    Awesome bro

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,48 @@
 <template>
   <div id="app" v-on:click="pressedBody">
     <nav role="navigation">
-      <!-- <div id="left-nav">
-      <a id="logo" href="/"><img src="./assets/logo.png"/></a>
+      <!--  Side navigation pop up -->
+      <div id="sideNav" class="sidenav" ref="sideNav">
+        <a href="javascript:void(0)" class="closebtn">Close</a>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/team">Team</router-link>
+        <router-link to="/upload">Upload</router-link>
+        <router-link to="/auth">Login</router-link>
+      </div>
+      <!--  Open button-->
+      <span style="font-size:30px;cursor:pointer" class="openbtn">&#9776;</span>
+      <!--  Logo  -->
+      <div class="logo-container">
+        <a id="logo" href="/"><img src="./assets/logo.png"/></a>
+      </div>
+      </nav>
+      <router-view></router-view>
     </div>
-    <div id="right-nav"> -->
+  </template>
 
-    <div id="mySidenav" class="sidenav" ref="sideNav">
-      <a href="javascript:void(0)" class="closebtn">&times;</a>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/team">Team</router-link>
-      <router-link to="/upload">Upload</router-link>
-      <router-link to="/auth">Login</router-link>
-    </div>
-    <span style="font-size:30px;cursor:pointer" class="openbtn">&#9776;</span>
-
-  </nav>
-  <router-view></router-view>
-</div>
-</template>
-
-<script>
-
-export default {
-  name: 'app',
-  data: function () {
-    return {
-      isNavOpen: false
-    }
-  },
-  methods: {
-    pressedBody: function (event) {
-      if (event.target.className === 'openbtn') {
-        this.$refs.sideNav.style.width = `250px`
-      } else {
-        this.$refs.sideNav.style.width = `0px`
+  <script>
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        isNavOpen: false
+      }
+    },
+    methods: {
+      pressedBody: function (event) {
+        if (event.target.className === 'openbtn') {
+          this.$refs.sideNav.style.width = `180px`
+        } else {
+          this.$refs.sideNav.style.width = `0px`
+        }
       }
     }
   }
-}
+  </script>
 
-</script>
+  <style lang="scss">
+  @import './styles/app';
+  @import './styles/nav';
 
-<style lang="scss">
-@import './styles/app';
-@import './styles/nav';
-
-</style>
+  </style>
